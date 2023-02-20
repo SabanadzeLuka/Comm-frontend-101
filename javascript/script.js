76f1ab2389ac7489f1acf0093aac88d6d6dd6f41
G-apxo <gioapkhadze@gmail.com>
2023-02-20 22:34:08 +0400

    123

---
+++ javascript/script.js
@@ -0,0 +1,161 @@
+// alert("GAMOTVLA CVLADIS")
+
+// var cvladi
+
+// alert(cvladi)
+
+// var erti, ori, sami
+// erti = 500
+// ori= 2
+// sami = 3
+
+
+/// DATA TYPES
+// sritng
+// Number
+// Boolean
+// undefined
+// null
+
+
+// sritng
+var demoOne = "This is String"
+console.log(demoOne)
+// Number
+var demoTwo = 5
+console.log(demoTwo)
+// Boolean
+var demoThree = true
+console.log(demoThree)
+
+
+var FirstNAme = "Tom"
+console.log(typeof(FirstNAme))
+
+var income = 12
+console.log(typeof(income))
+
+var undefinedVariable 
+console.log(typeof(undefinedVariable))
+
+// var x = 10
+
+// var y = x * 19;
+
+// console.log(y)
+
+// var x = 41;
+// var y = 7;
+
+// var z = x % 7;
+
+// console.log(z)
+
+// var x = 5 
+// x *= 5 // x = x * 5
+// console.log(x)
+
+// var income = 100
+// var strIncome = "100"
+
+// var result = income == strIncome
+// var result1 = income === strIncome // 100 === 100 da number === string
+// console.log(result) // true
+// console.log(result1) // false
+
+
+// var income = 100
+// var isDeosit = true
+
+// var result = income > 50 || isDeosit == true;
+// console.log(result) // 
+
+// var income = 100
+// var result1 = !(income >= 100)
+
+// console.log(result1) // false 
+
+// var isDeposit = true
+// var result = !isDeposit
+
+// console.log(result) // false
+
+// var name = "Tom"
+// var surname = "Rame"
+
+// var fullname = name + surname
+
+// console.log(fullname)
+
+// console.log(3 + 3 + "3")
+
+
+// var number1 = "45.08";
+
+// var number2 = "5.4";
+
+// var result = parseFloat(number1) + parseFloat(number2) ;
+
+// console.log(result)
+
+// var number3 = "123hello"
+
+// var number4 = parseInt(number3)
+
+// console.log(number4)
+
+// var strSum = prompt("შეიყვანეთ ანაბრის თანხა", 1000 )
+
+// var strPercent = prompt( "შეიყვანეთ საპროცენტო განაკვეთი", 10)
+
+// var sum = parseInt (strSum);
+
+// var percent = parseInt(strPercent)
+
+// sum = sum + sum * percent / 100
+
+// alert("პროცენტის დარიცხვის შემდეგ ანარბის თანხა" + sum)
+
+// var income = 100
+
+// if(income < 50) {
+//     alert("დაბალი შემოსავალი")
+// }
+// else{
+//     alert("kai xelpasi")
+// }
+
+// var income = prompt("შეიყვანეთ თქვენი ხელფასი")
+
+// if (income < 200){
+//     alert("საშუალოზე დაბალი შემოსავალი")
+// }
+// else if (income >= 200 && income <400){
+//     alert("საშუალო ხელფასი")
+// }
+// else if (income >=400 && income < 1000){
+//     alert("კაი ხელფასი")
+// }
+// else{
+//     alert("შენ შემოგევლე მე")
+// }
+
+
+
+var numbers1 = [0,1,2,3,4,5] // ერთგანზომიელბაინი
+
+var numbers2 = [[0,1,2], [3,4,5]] // ორგანზომილებიანი
+
+
+
+//0,1,2,3,4,5
+
+// 0,1,2
+// 3,4,5
+
+var people=[
+    ["Tom", 25, false]
+    ["Alice", 22, true]
+    ["Bob", 28, false]
+]
+console.log(people[0])
